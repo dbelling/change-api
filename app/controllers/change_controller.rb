33022cf125f4300d77bdba88d0195eaa402ccfec
Dan Belling <danhbelling@gmail.com>
2017-10-22 21:25:28 -0500

    Gluing service to controller actions

--- app/controllers/change_controller.rb
+++ app/controllers/change_controller.rb
@@ -1,13 +1,26 @@
 class ChangeController < ApplicationController
+  before_action :parse_change
+
   def show
-    render json: {}, status: :ok
+    render json: @quantities.to_json
   end
 
   def create
-    render json: {}, status: :created
+    render json: @quantities.to_json, status: :created
   end
 
   def index
     render json: {'message': 'Welcome to the Change API!'}, status: :ok
+  end
+
+  private
+
+  def change_params
+    params.permit(:amount)
+  end
+
+  def parse_change
+    @change_amount = change_params[:amount].to_f
+    @quantities = ChangeService.make_change(@change_amount)
   end
 end
